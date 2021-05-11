277b837b700e5622f7e42d5fd8f7e660af96ad20
Ashish2030 <Kumarashish79924@gmail.com>
2021-05-11 06:10:41 +0530

    𝟏𝐬𝐭_𝐜𝐨𝐦𝐦𝐢𝐭

---
+++ src/main/java/com/exam/exmaportal/ExmaportalApplication.java
@@ -0,0 +1,13 @@
+package com.exam.exmaportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExmaportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExmaportalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exam/exmaportal/ExmaportalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exam.exmaportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExmaportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
